c8edf1a176fbee5cfb2cddb72a32f5ff30e4440f
hoaithuong2002 <dgth2o2@gmail.com>
2021-06-16 14:59:12 +0700

    bai tap nang cap hackernews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ArticleComponent } from './article/article.component';
 import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    LikeComponent
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
